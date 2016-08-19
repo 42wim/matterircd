2ef961c2b85d805204c8d9f659b2961649c272be
Wim <wim@42.be>
2016-08-19 23:33:06 +0200

    Release v0.9.1

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ import (
 
 var flagRestrict, flagDefaultTeam, flagDefaultServer, flagTLSBind, flagTLSDir *string
 var flagInsecure *bool
-var version = "0.9.0"
+var version = "0.9.1"
 var logger *logrus.Entry
 
 func main() {
