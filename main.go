748e87887219ad2012a6046739b5cc2679961a77
Wim <wim@42.be>
2016-09-25 17:45:16 +0200

    Sync with mattermost 3.4.0

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ import (
 
 var flagRestrict, flagDefaultTeam, flagDefaultServer, flagTLSBind, flagTLSDir *string
 var flagInsecure *bool
-var version = "0.10.2"
+var version = "0.11-dev"
 var logger *logrus.Entry
 
 func main() {
