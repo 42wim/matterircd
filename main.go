d3ff00aa2564d3e8ca60c034247cc93606bf72df
Wim <wim@42.be>
2017-01-03 22:40:26 +0100

    Release v0.11.1

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ import (
 
 var flagRestrict, flagDefaultTeam, flagDefaultServer, flagTLSBind, flagTLSDir *string
 var flagInsecure *bool
-var version = "0.11.1-dev"
+var version = "0.11.1"
 var logger *logrus.Entry
 
 func main() {
