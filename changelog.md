21dd4d193710e03d0e6a35487ee23298ee2c2f47
Wim <wim@42.be>
2018-02-12 10:17:41 +0100

    Release v0.16.2

--- changelog.md
+++ changelog.md
@@ -1,3 +1,7 @@
+# v0.16.2
+## Bugfix
+* Remove crash on channel lookup of private messages
+
 # v0.16.1
 ## Bugfix
 * Remove debug code which could cause a crash
--- main.go
+++ main.go
@@ -15,7 +15,7 @@ import (
 var (
 	flagRestrict, flagDefaultTeam, flagDefaultServer, flagTLSBind, flagTLSDir *string
 	flagInsecure, flagSkipTLSVerify                                           *bool
-	version                                                                   = "0.16.1"
+	version                                                                   = "0.16.2"
 	githash                                                                   string
 	logger                                                                    *logrus.Entry
 	cfg                                                                       config.Config
