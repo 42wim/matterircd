95b39a8bbd1821e0ed921d706065d74b7d9e0ff3
Wim <wim@42.be>
2019-01-25 15:37:34 +0100

    Bump version

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ import (
 var (
 	flagRestrict, flagDefaultTeam, flagDefaultServer, flagTLSBind, flagTLSDir *string
 	flagInsecure, flagSkipTLSVerify                                           *bool
-	version                                                                   = "0.19.0"
+	version                                                                   = "0.19.1-dev"
 	githash                                                                   string
 	logger                                                                    *logrus.Entry
 	cfg                                                                       config.Config
