282f35f1dcd2fc54bed29aa9a204077f89d842be
Wim <wim@42.be>
2019-12-08 23:43:23 +0100

    Release v0.19.4

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ import (
 var (
 	flagRestrict, flagDefaultTeam, flagDefaultServer, flagTLSBind, flagTLSDir *string
 	flagInsecure, flagSkipTLSVerify                                           *bool
-	version                                                                   = "0.19.3"
+	version                                                                   = "0.19.4"
 	githash                                                                   string
 	logger                                                                    *logrus.Entry
 	cfg                                                                       config.Config
