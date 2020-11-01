1f2438ecd05f8cd78e18643022e96286c79a05fe
Wim <wim@42.be>
2020-11-01 16:30:42 +0100

    Use override_username when available in backlog (mattermost) (#343)

--- mm-go-irckit/userbridge.go
+++ mm-go-irckit/userbridge.go
@@ -562,9 +562,15 @@ func (u *User) addUserToChannelWorker(channels <-chan *bridge.ChannelInfo, throt
 
 			ts := time.Unix(0, p.CreateAt*int64(time.Millisecond))
 
+			props := p.GetProps()
+			botname, override := props["override_username"].(string)
+
 			for _, post := range strings.Split(p.Message, "\n") {
 				user := u.br.GetUser(p.UserId)
 				nick := user.Nick
+				if override {
+					nick = botname
+				}
 				shortdate := ts.Format("2006-01-02")
 				date := ts.Format("2006-01-02 15:04:05")
 				if shortdate != prevDate {
