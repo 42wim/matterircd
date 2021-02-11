42wim/matterircd

commit c68410c591616b3ab04cb2d766b57798437b6fb7

Fix golangci-lint dupl

mm-go-irckit/server_commands.go
@@ -606,27 +606,7 @@ func threadMsgChannel(u *User, msg *irc.Message, channelID string) bool {
 }
 
 func threadMsgUser(u *User, msg *irc.Message, toUser string) bool {
-	threadID, text := parseThreadID(u, msg, toUser)
-	if threadID == "" {
-		return false
-	}
-
-	msgID, err := u.br.MsgUserThread(toUser, threadID, text)
-	if err != nil {
-		u.MsgSpoofUser(u, u.br.Protocol(), "msg: "+text+" could not be send: "+err.Error())
-		return false
-	}
-
-	u.msgLastMutex.Lock()
-	defer u.msgLastMutex.Unlock()
-	u.msgLast[toUser] = [2]string{msgID, threadID}
-	u.saveLastViewedAt(toUser)
-
-	if u.v.GetBool(u.br.Protocol()+".prefixcontext") || u.v.GetBool(u.br.Protocol()+".suffixcontext") {
-		u.prefixContext(toUser, msgID, "", "")
-	}
-
-	return true
+	return threadMsgChannel(u, msg, toUser)
 }
 
 // CmdQuit is a handler for the /QUIT command.

commit b8558828d7e443ba14e6e37cf1156e0b38e4f6e9

Fix permissions issues when replying to threads in DM
This was introduced in commit c68410c5 where threadMsgUser() now calls
threadMsgChannel() which then incorrectly calls u.br.MsgChannelThread().

mm-go-irckit/server_commands.go
@@ -581,13 +581,19 @@ func parseThreadID(u *User, msg *irc.Message, channelID string) (string, string)
 	return "", ""
 }
 
-func threadMsgChannel(u *User, msg *irc.Message, channelID string) bool {
+func threadMsgChannelUser(u *User, msg *irc.Message, channelID string, toUser bool) bool {
 	threadID, text := parseThreadID(u, msg, channelID)
 	if threadID == "" {
 		return false
 	}
 
-	msgID, err := u.br.MsgChannelThread(channelID, threadID, text)
+	var msgID string
+	var err error
+	if toUser {
+		msgID, err = u.br.MsgUserThread(channelID, threadID, text)
+	} else {
+		msgID, err = u.br.MsgChannelThread(channelID, threadID, text)
+	}
 	if err != nil {
 		u.MsgSpoofUser(u, u.br.Protocol(), "msg: "+text+" could not be send: "+err.Error())
 		return false
@@ -605,8 +611,12 @@ func threadMsgChannel(u *User, msg *irc.Message, channelID string) bool {
 	return true
 }
 
+func threadMsgChannel(u *User, msg *irc.Message, channelID string) bool {
+	return threadMsgChannelUser(u, msg, channelID, false)
+}
+
 func threadMsgUser(u *User, msg *irc.Message, toUser string) bool {
-	return threadMsgChannel(u, msg, toUser)
+	return threadMsgChannelUser(u, msg, toUser, true)
 }
 
 // CmdQuit is a handler for the /QUIT command.