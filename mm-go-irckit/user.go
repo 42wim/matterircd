42wim/matterircd

commit bc88bb1bef9936127551db69912333fb8e81e458

Flush buffers on reactions, replies to threads or message modifications (#387)

mm-go-irckit/user.go
@@ -3,6 +3,7 @@ package irckit
 import (
 	"fmt"
 	"net"
+	"regexp"
 	"strings"
 	"sync"
 	"time"
@@ -180,9 +181,10 @@ func (u *User) Decode() {
 					// start timer now
 					t.Reset(time.Duration(bufferTimeout) * time.Millisecond)
 				} else {
-					if strings.HasPrefix(msg.Trailing, "\x01ACTION") {
+					re := regexp.MustCompile(`^(?:\@\@|s/)(?:[0-9a-f]{3}|[0-9a-z]{26}|!!)|/`)
+					if strings.HasPrefix(msg.Trailing, "\x01ACTION") || re.MatchString(msg.Trailing) {
 						// flush buffer
-						logger.Debug("flushing buffer because of /me")
+						logger.Debug("flushing buffer because of /me, replies to threads, and message modifications")
 						u.BufferedMsg.Trailing = strings.TrimSpace(u.BufferedMsg.Trailing)
 						u.DecodeCh <- u.BufferedMsg
 						u.BufferedMsg = nil

commit c1a264b6c7a14838e878148d5548bd285546216d

Fixed buffer flushing
Bug introduced in commit bc88bb1. Flushing of buffers would happen
when text contains '/' anywhere, and not just at the start for
actions.

mm-go-irckit/user.go
@@ -181,7 +181,7 @@ func (u *User) Decode() {
 					// start timer now
 					t.Reset(time.Duration(bufferTimeout) * time.Millisecond)
 				} else {
-					re := regexp.MustCompile(`^(?:\@\@|s/)(?:[0-9a-f]{3}|[0-9a-z]{26}|!!)|/`)
+					re := regexp.MustCompile(`^(?:\@\@|s/)(?:[0-9a-f]{3}|[0-9a-z]{26}|!!)|^/`)
 					if strings.HasPrefix(msg.Trailing, "\x01ACTION") || re.MatchString(msg.Trailing) {
 						// flush buffer
 						logger.Debug("flushing buffer because of /me, replies to threads, and message modifications")