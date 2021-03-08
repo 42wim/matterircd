42wim/matterircd

commit bc88bb1bef9936127551db69912333fb8e81e458

Flush buffers on reactions, replies to threads or message modifications (#387)

mm-go-irckit/user.go
@@ -3,6 +3,7 @@ package irckit
 import (
 	"fmt"
 	"net"
+	"regexp"
 	"strings"
 	"sync"
 	"time"
@@ -180,9 +181,10 @@ func (u *User) Decode() {
 					// start timer now
 					t.Reset(time.Duration(bufferTimeout) * time.Millisecond)
 				} else {
-					if strings.HasPrefix(msg.Trailing, "\x01ACTION") {
+					re := regexp.MustCompile(`^(?:\@\@|s/)(?:[0-9a-f]{3}|[0-9a-z]{26}|!!)|/`)
+					if strings.HasPrefix(msg.Trailing, "\x01ACTION") || re.MatchString(msg.Trailing) {
 						// flush buffer
-						logger.Debug("flushing buffer because of /me")
+						logger.Debug("flushing buffer because of /me, replies to threads, and message modifications")
 						u.BufferedMsg.Trailing = strings.TrimSpace(u.BufferedMsg.Trailing)
 						u.DecodeCh <- u.BufferedMsg
 						u.BufferedMsg = nil

commit 60571ebb1b75b645fa1035104328019c560e8782

Fix buffer flushing yet again
Yet another fix to bug introduced by commit bc88bb1. Still an issue
with pasting paths causing the buffers to be flushed when it shouldn't
(e.g. /srv/mydir). Instead, split it into two regexs to avoid
confusion. One for replies to message/threads as well as reactions and
the other for message modifications.

mm-go-irckit/user.go
@@ -181,8 +181,9 @@ func (u *User) Decode() {
 					// start timer now
 					t.Reset(time.Duration(bufferTimeout) * time.Millisecond)
 				} else {
-					re := regexp.MustCompile(`^(?:\@\@|s/)(?:[0-9a-f]{3}|[0-9a-z]{26}|!!)|^/`)
-					if strings.HasPrefix(msg.Trailing, "\x01ACTION") || re.MatchString(msg.Trailing) {
+					replyRe := regexp.MustCompile(`\@\@(?:[0-9a-z]{26}|[0-9a-f]{3}|!!)\s`)
+					modifyRe := regexp.MustCompile(`^s/(?:[0-9a-z]{26}|[0-9a-f]{3}|!!)?/`)
+					if strings.HasPrefix(msg.Trailing, "\x01ACTION") || replyRe.MatchString(msg.Trailing) || modifyRe.MatchString(msg.Trailing) {
 						// flush buffer
 						logger.Debug("flushing buffer because of /me, replies to threads, and message modifications")
 						u.BufferedMsg.Trailing = strings.TrimSpace(u.BufferedMsg.Trailing)