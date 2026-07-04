12bbfaa0c64effe06387327eb2ce15ffbd0b352d
Haw Loeung <haw.loeung@canonical.com>
2026-07-04 14:34:58 +1000

    Bump Go version 1.21 -> 1.22 for all the new goodness (#661)
    * Bump Go version 1.21 -> 1.22 for all the new goodness
    
    * Fix golangci-lint for loop use integer range (Go 1.22+) (intrange)
    
    * Reduce check alive logging noise
    
    * Log uptime on websocket is active as well

--- vendor/github.com/matterbridge/matterclient/matterclient.go
+++ vendor/github.com/matterbridge/matterclient/matterclient.go
@@ -584,27 +584,26 @@ func (m *Client) doCheckAlive() error {
 		return fmt.Errorf("websocket listen error: %w", m.WsClient.ListenError)
 	}
 
+	connectedUnix := m.connectedAt.Load()
+	uptime := time.Since(time.Unix(connectedUnix, 0)).Round(time.Second)
 	lastActiveUnix := m.lastWsActivity.Load()
 	timeSinceActivity := time.Since(time.Unix(lastActiveUnix, 0))
 
 	if timeSinceActivity < 20*time.Second {
-		m.logger.Debugf("websocket is active (last event %v ago), skipping ping", timeSinceActivity.Round(time.Second))
+		m.logger.Tracef("websocket is active (last event %v ago; up %s), skipping ping", timeSinceActivity.Round(time.Second), uptime)
 		return nil
 	}
-
-	connectedUnix := m.connectedAt.Load()
-	uptime := time.Since(time.Unix(connectedUnix, 0)).Round(time.Second)
 
 	if timeSinceActivity < 55*time.Second {
 		// Send a ping down the websocket to try to keep it active/alive
 		if m.WsClient != nil {
-			m.logger.Debugf("websocket has been quiet (last event %v ago; up %s), sending websocket ping", timeSinceActivity.Round(time.Second), uptime)
+			m.logger.Tracef("websocket has been quiet (last event %v ago; up %s), sending websocket ping", timeSinceActivity.Round(time.Second), uptime)
 			m.WsClient.SendMessage("ping", nil)
 			return nil
 		}
 	}
 
-	m.logger.Debugf("websocket has been quiet (last event %v ago; up %s), falling back to HTTP GetPing", timeSinceActivity.Round(time.Second), uptime)
+	m.logger.Tracef("websocket has been quiet (last event %v ago; up %s), falling back to HTTP GetPing", timeSinceActivity.Round(time.Second), uptime)
 	if _, _, err := m.Client.GetPing(); err != nil {
 		m.logger.Warnf("fallback HTTP ping failed (up %s): %w", uptime, err)
 		return fmt.Errorf("fallback HTTP ping failed (up %s): %w", uptime, err)
