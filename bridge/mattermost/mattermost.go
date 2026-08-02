14bbcef3b47c6ec25200c0c4909d125ec75210d6
Haw Loeung <haw.loeung@canonical.com>
2026-08-03 09:50:09 +1000

    golangci-lint fixes

--- bridge/mattermost/mattermost.go
+++ bridge/mattermost/mattermost.go
@@ -161,7 +161,7 @@ func (m *Mattermost) loginToMattermost(onWsConnect func()) (*matterclient.Client
 	return mc, nil
 }
 
-//nolint:cyclop
+//nolint:cyclop,gocognit,gocyclo
 func (m *Mattermost) handleWsMessage(quitChan chan struct{}, workerIdx int) {
 	for {
 		if m.mc.WsQuit {
